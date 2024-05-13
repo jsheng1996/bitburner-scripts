6b71a0b3c91bf53d6a56c4be14d0362d362e758f
jsheng1996 <jsheng1996.work@gmail.com>
2024-05-13 17:25:53 +0800

    Update corp_shares.js

--- corp/corp_shares.js
+++ corp/corp_shares.js
@@ -1,18 +1,18 @@
 export async function main(ns) {
   //Logging
   ns.disableLog('ALL'); ns.tail();
-  ns.print("Corpo shares script started")
+  ns.print("Corpo shares script started");
 
   var corp = ns.corporation.getCorporation();
   if (corp.dividendRate == 1){
     ns.corporation.buyBackShares(ns.corporation.getCorporation().numShares);
     ns.corporation.issueDividends(0);
-    ns.print("Bought" + ns.corporation.getCorporation().numShares/2000000 +"m shares")
-    ns.print("Set div rate to 0%")
+    ns.print("Bought" + ns.corporation.getCorporation().numShares/2000000 +"m shares");
+    ns.print("Set div rate to 0%");
   } else{
     ns.corporation.sellShares(ns.corporation.getCorporation().numShares/2);
     ns.corporation.issueDividends(0.9);
-    ns.print("Sold" + ns.corporation.getCorporation().numShares*2/1000000 +"m shares")
-    ns.print("Set div rate to 90%")
+    ns.print("Sold" + ns.corporation.getCorporation().numShares*2/1000000 +"m shares");
+    ns.print("Set div rate to 90%");
   }
 }
