aea86ad37a833f9f76c57d924479f08f6b1809da
jsheng1996 <jsheng1996.work@gmail.com>
2024-05-15 15:24:14 +0800

    Update corp_shares.js

--- corp/corp_shares.js
+++ corp/corp_shares.js
@@ -7,12 +7,12 @@ export async function main(ns) {
   if (corp.dividendRate == 0.9){
     ns.corporation.buyBackShares(ns.corporation.getCorporation().numShares);
     ns.corporation.issueDividends(0);
-    ns.print("Bought" + ns.corporation.getCorporation().numShares/2000000 +"m shares");
+    ns.print("Bought " + ns.corporation.getCorporation().numShares/2000000 +"m shares");
     ns.print("Set div rate to 0%");
   } else{
     ns.corporation.sellShares(ns.corporation.getCorporation().numShares/2);
     ns.corporation.issueDividends(0.9);
-    ns.print("Sold" + ns.corporation.getCorporation().numShares*/1000000 +"m shares");
+    ns.print("Sold " + ns.corporation.getCorporation().numShares/1000000 +"m shares");
     ns.print("Set div rate to 90%");
   }
 }
